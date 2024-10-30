d0b1c1a6c23012924d38330b1f023a689073cadb
Roman Uraykin <UraykinRab@yandex.ru>
2024-10-30 23:06:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/musicapp/FavoriteFragment.kt
@@ -0,0 +1,21 @@
+package com.example.musicapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.musicapp.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment: Fragment() {
+    private lateinit var binding: FragmentFavoriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
